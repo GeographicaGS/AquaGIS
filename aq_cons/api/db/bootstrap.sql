bf1a2a932e3b784bf675e128888e55aa66dffe72
José M. Camacho <hello@josemazo.com>
2017-12-26 17:37:03 +0100

    Forecast DDL - WIP

---
+++ aq_cons/api/db/bootstrap.sql
@@ -0,0 +1,9 @@
+/*
+ * Script to load all PL/PgSQL functions
+ */
+
+-- AquaGIS Consumption functions
+-- \ir common/urbo_aq_cons_function_name.sql
+
+-- DDL
+\ir common/ddl/urbo_createtables_aq_cons.sql
