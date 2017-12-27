6a299945479f49ab812e61eab47401271e6586d0
José M. Camacho <hello@josemazo.com>
2017-12-27 12:03:13 +0100

    Finished first version of aq_cons metadata

--- aq_cons/api/db/bootstrap.sql
+++ aq_cons/api/db/bootstrap.sql
@@ -5,5 +5,6 @@
 -- AquaGIS Consumption functions
 -- \ir common/urbo_aq_cons_function_name.sql
 
--- DDL
+-- DDL & DML
 \ir common/ddl/urbo_createtables_aq_cons.sql
+\ir common/dml/urbo_createmetadata_aq_cons.sql
