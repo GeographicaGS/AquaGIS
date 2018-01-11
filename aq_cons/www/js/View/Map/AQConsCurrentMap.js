5dc519a2a88af4651964392ff62994746cf40a7d
Ivan Moreno <ivan@geographica.gs>
2018-01-11 14:43:50 +0100

    new style for map

--- aq_cons/www/js/View/Map/AQConsCurrentMap.js
+++ aq_cons/www/js/View/Map/AQConsCurrentMap.js
@@ -69,7 +69,7 @@ App.View.Panels.Aq_cons.CurrentMap = App.View.Map.MapboxView.extend({
         'type': 'fill',
         'source': 'aqua_plots',
         'layout': {},
-        'minzoom': 18,
+        'minzoom': 16,
         'paint': {
             'fill-color': '#165288',
             'fill-opacity': 0.4
@@ -89,65 +89,58 @@ App.View.Panels.Aq_cons.CurrentMap = App.View.Map.MapboxView.extend({
         'type': 'line',
         'source': 'supply_line_datasource',
         'paint': {
-          'line-color': '#3561BA',
+          'line-color': '#45AAB0',
         }
       }],
       map: this
     });
 
-    this._sensorLayer = new App.View.Map.Layer.Aq_cons.GenericLayer({
+    this._wellLineLayer = new App.View.Map.Layer.Aq_cons.GenericLayer({
       source: {
-        id: 'sensors_datasource',
-        model: sensor,
+        id: 'well_line_datasource',
+        model: wellLine,
         payload: this._payload
       },
       layers:[{
-        'id': 'sensors_circle',
-        'type': 'circle',
-        'source': 'sensors_datasource',
-        'maxzoom': 18,
+        'id': 'well_line',
+        'type': 'line',
+        'source': 'well_line_datasource',
         'paint': {
-          'circle-radius': 2,
-          'circle-color': '#8672D2',
-        }
-      }, {
-        'id': 'sensors_symbol',
-        'type': 'symbol',
-        'source': 'sensors_datasource',
-        'minzoom': 18,
-        'layout': {
-          'icon-size': 1.5,
-          'icon-image': 'sensor-agua',
-          'icon-allow-overlap': true
+          'line-color': '#0F82E0',
         }
       }],
       map: this
     });
 
-    this._tankLayer = new App.View.Map.Layer.Aq_cons.GenericLayer({
+    this._hydrantLineLayer = new App.View.Map.Layer.Aq_cons.GenericLayer({
       source: {
-        id: 'tanks_datasource',
-        model: tank,
+        id: 'hydrant_line_datasource',
+        model: hydrantLine,
         payload: this._payload
       },
       layers:[{
-        'id': 'tanks_circle',
-        'type': 'circle',
-        'source': 'tanks_datasource',
-        'maxzoom': 18,
+        'id': 'hydrant_line',
+        'type': 'line',
+        'source': 'hydrant_line_datasource',
         'paint': {
-          'circle-radius': 2,
-          'circle-color': '#68BEE2',
+          'line-color': '#6D83E9',
         }
-      }, {
-        'id': 'tanks_symbol',
-        'type': 'symbol',
-        'source': 'tanks_datasource',
-        'minzoom': 18,
-        'layout': {
-          'icon-size': 1.5,
-          'icon-image': 'deposito',
-          'icon-allow-overlap': true
+      }],
+      map: this
+    });
+
+    this._valveLineLayer = new App.View.Map.Layer.Aq_cons.GenericLayer({
+      source: {
+        id: 'valve_line_datasource',
+        model: valveLine,
+        payload: this._payload
+      },
+      layers:[{
+        'id': 'valve_line',
+        'type': 'line',
+        'source': 'valve_line_datasource',
+        'paint': {
+          'line-color': '#68BC84',
         }
       }],
       map: this
@@ -164,16 +157,18 @@ App.View.Panels.Aq_cons.CurrentMap = App.View.Map.MapboxView.extend({
         'type': 'circle',
         'source': 'connections_datasource',
         'minzoom': 14,
-        'maxzoom': 18,
+        'maxzoom': 16,
         'paint': {
           'circle-radius': 2,
-          'circle-color': '#45AAB0',
+          'circle-stroke-width': 1,
+          'circle-stroke-color': '#DDDDDF',
+          'circle-color': '#3561BA',
         }
       }, {
         'id': 'connections_symbol',
         'type': 'symbol',
         'source': 'connections_datasource',
-        'minzoom': 18,
+        'minzoom': 16,
         'layout': {
           'icon-size': 1.5,
           'icon-image': 'acometida',
@@ -194,16 +189,18 @@ App.View.Panels.Aq_cons.CurrentMap = App.View.Map.MapboxView.extend({
         'type': 'circle',
         'source': 'hydrants_datasource',
         'minzoom': 14,
-        'maxzoom': 18,
+        'maxzoom': 16,
         'paint': {
           'circle-radius': 2,
-          'circle-color': '#68BC84',
+          'circle-stroke-width': 1,
+          'circle-stroke-color': '#DDDDDF',
+          'circle-color': '#6D83E9',
         }
       }, {
         'id': 'hydrants_symbol',
         'type': 'symbol',
         'source': 'hydrants_datasource',
-        'minzoom': 18,
+        'minzoom': 16,
         'layout': {
           'icon-size': 1.5,
           'icon-image': 'hidrante',
@@ -224,16 +221,18 @@ App.View.Panels.Aq_cons.CurrentMap = App.View.Map.MapboxView.extend({
         'type': 'circle',
         'source': 'valves_datasource',
         'minzoom': 14,
-        'maxzoom': 18,
+        'maxzoom': 16,
         'paint': {
           'circle-radius': 2,
-          'circle-color': '#BF9943',
+          'circle-stroke-width': 1,
+          'circle-stroke-color': '#DDDDDF',
+          'circle-color': '#68BC84',
         }
       }, {
         'id': 'valves_symbol',
         'type': 'symbol',
         'source': 'valves_datasource',
-        'minzoom': 18,
+        'minzoom': 16,
         'layout': {
           'icon-size': 1.5,
           'icon-image': 'valvula',
@@ -242,43 +241,102 @@ App.View.Panels.Aq_cons.CurrentMap = App.View.Map.MapboxView.extend({
       }],
       map: this
     });
+
+    this._wellLayer = new App.View.Map.Layer.Aq_cons.GenericLayer({
+      source: {
+        id: 'wells_datasource',
+        model: well,
+        payload: this._payload
+      },
+      layers:[{
+        'id': 'wells_circle',
+        'type': 'circle',
+        'source': 'wells_datasource',
+        'minzoom': 14,
+        'maxzoom': 16,
+        'paint': {
+          'circle-radius': 2,
+          'circle-stroke-width': 1,
+          'circle-stroke-color': '#DDDDDF',
+          'circle-color': '#0F82E0',
+        }
+      }, {
+        'id': 'wells_symbol',
+        'type': 'symbol',
+        'source': 'wells_datasource',
+        'minzoom': 16,
+        'layout': {
+          'icon-size': 1.5,
+          'icon-image': 'pozo',
+          'icon-allow-overlap': true
+        }
+      }],
+      map: this
+    });
+
+    this._sensorLayer = new App.View.Map.Layer.Aq_cons.GenericLayer({
+      source: {
+        id: 'sensors_datasource',
+        model: sensor,
+        payload: this._payload
+      },
+      layers:[{
+        'id': 'sensors_circle',
+        'type': 'circle',
+        'source': 'sensors_datasource',
+        'maxzoom': 16,
+        'paint': {
+          'circle-radius': 2,
+          'circle-stroke-width': 1,
+          'circle-stroke-color': '#DDDDDF',
+          'circle-color': '#8672D2',
+        }
+      }, {
+        'id': 'sensors_symbol',
+        'type': 'symbol',
+        'source': 'sensors_datasource',
+        'minzoom': 16,
+        'layout': {
+          'icon-size': 1.5,
+          'icon-image': 'sensor-agua',
+          'icon-allow-overlap': true
+        }
+      }],
+      map: this
+    });
+
+    this._tankLayer = new App.View.Map.Layer.Aq_cons.GenericLayer({
+      source: {
+        id: 'tanks_datasource',
+        model: tank,
+        payload: this._payload
+      },
+      layers:[{
+        'id': 'tanks_circle',
+        'type': 'circle',
+        'source': 'tanks_datasource',
+        'maxzoom': 16,
+        'paint': {
+          'circle-radius': 2,
+          'circle-stroke-width': 1,
+          'circle-stroke-color': '#DDDDDF',
+          'circle-color': '#68BEE2',
+        }
+      }, {
+        'id': 'tanks_symbol',
+        'type': 'symbol',
+        'source': 'tanks_datasource',
+        'minzoom': 16,
+        'layout': {
+          'icon-size': 1.5,
+          'icon-image': 'deposito',
+          'icon-allow-overlap': true
+        }
+      }],
+      map: this
+    });
   
-    // this._connectionLineLayer = new App.View.Map.Layer.ConnectionLineLayer(connectionLine, {
-    //   filters: {
-    //     condition: {}
-    //   }
-    // }, this._mapInstance);
-    // this._supplyLayer = new App.View.Map.Layer.SupplyLayer(supply, {
-    //   filters: {
-    //     condition: {}
-    //   }
-    // }, this._mapInstance);
-    // this._hydrantLineLayer = new App.View.Map.Layer.HydrantLineLayer(hydrantLine, {
-    //   filters: {
-    //     condition: {}
-    //   }
-    // }, this._mapInstance);
     // this._valveLineLayer = new App.View.Map.Layer.ValveLineLayer(valveLine, {
-    //   filters: {
-    //     condition: {}
-    //   }
-    // }, this._mapInstance);
-    // this._wellLayer = new App.View.Map.Layer.WellLayer(well, {
-    //   filters: {
-    //     condition: {}
-    //   }
-    // }, this._mapInstance);
-    // this._wellLineLayer = new App.View.Map.Layer.WellLineLayer(wellLine, {
-    //   filters: {
-    //     condition: {}
-    //   }
-    // }, this._mapInstance);
-    // this._plotLayer = new App.View.Map.Layer.PlotLayer(plot, {
-    //   filters: {
-    //     condition: {}
-    //   }
-    // }, this._mapInstance);
-    // this._plotLineLayer = new App.View.Map.Layer.PlotStructureLayer(plotStructure, {
     //   filters: {
     //     condition: {}
     //   }
