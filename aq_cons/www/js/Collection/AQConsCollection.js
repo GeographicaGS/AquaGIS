5ef48d0362bd07dd7f0d5590e5af554e4cd3bd51
Raúl Yeguas <neokore@gmail.com>
2018-01-12 13:15:08 +0100

    Consume forecast widget

--- aq_cons/www/js/Collection/AQConsCollection.js
+++ aq_cons/www/js/Collection/AQConsCollection.js
@@ -18,41 +18,9 @@ App.Collection.Aq_cons.PanelList = Backbone.Collection.extend({
   }
 });
 
-App.Collection.Aq_cons.ConsumptionStacked = Backbone.Collection.extend({
-  initialize: function(models,options) {
-      this.options = options;
-  },
-
-  url: function(){
-    return App.config.api_url + '/' + this.options.scope +'/variables/timeserie'
-  },
-
-  parse: function(response) {
-    response = _.map(response, function(r){
-      return {'step':r.name, 'elements':_.filter(r.values, function(r) {return r.name!='closed'})};
-    });
-    response = _.each(response,function(r) {
-      r.elements = _.map(r.elements, function(el){
-        var json = {};
-        json[el.name] = el.total;
-        return json;
-      });
-    });
-
-    return response;
-  },
-
-  fetch: function(options) {
-    if(!options)
-      options = {};
-
-    var date = App.ctx.getDateRange();
-    options['data'] = {
-      'start': date.start,
-      'finish': date.finish
-    };
-
-    return Backbone.Collection.prototype.fetch.call(this, options);
-  }
-
-});
+App.Static.Collection.Aq_cons.LandUses = new Backbone.Collection([
+  {id: 'domestic', name: __('Doméstica'), color: '#CB727E'},
+  {id: 'industrial', name: __('Industrial'), color: '#4ED8D8'},
+  {id: 'comercial', name: __('Comercial'), color: '#9AC74A'},
+  {id: 'public', name: __('Público'), color: '#E8BA4C'},
+]);
--- aq_cons/www/js/View/Widgets/ConsumptionForecastByLandUse.js
+++ aq_cons/www/js/View/Widgets/ConsumptionForecastByLandUse.js
@@ -28,34 +28,32 @@ App.View.Widgets.Aq_cons.ConsumptionForecastByLandUse = App.View.Widgets.Base.ex
       }
     });
 
-    // this._chartModel = new Backbone.Model({
-    //   'scope': this.options.id_scope,
-    //   'colors':['#E8BA4C', '#4ED8D8', '#9AC74A', '#CB727E'],
-    //   'xAxisFunction': function(d) {
-    //     return __(d);
-    //   },
-    //   'yAxisLabel': __('Consumo (m³)'),
-    //   'hideTooltip':true,
-    //   'showLegend': true,
-    //   'legend': [__('Público'), __('Industrial'), __('Comercial'), __('Doméstica')]
-    // });
-
     this._chartModel = new App.Model.BaseChartConfigModel({
       stacked: true,
-      colors: ['#E8BA4C', '#4ED8D8', '#9AC74A', '#CB727E'],
-      xAxisFunction: function(d) {
-        return __(d);
+      colors: function(d,index){
+        var type = App.Static.Collection.Aq_cons.LandUses.get(d.realKey);
+        return type.get('color');
+      },
+      xAxisFunction: function (d) {
+        return __('Todos los sectores');
+      },
+      yAxisFunction: function(d){
+        return App.nbf(d / 1000, {decimals:0});
       },
       yAxisLabel: __('Consumo (m³)'),
       legendNameFunc: function (d) {
         return __(d);
       },
       legendTemplate: this._template_legend,
+      legendNameFunc: function(d){
+        var type = App.Static.Collection.Aq_cons.LandUses.get(d);
+        return type.get('name');
+      },
       formatYAxis: {
         numberOfValues: 4,
         tickFormat: function (d) {
           var unit = 'm³';
-          var value = App.nbf(d);
+          var value = App.nbf(d / 1000, {decimals:0});
           if (domain && d === domain[1]) {
             value += unit;
           }
@@ -63,6 +61,11 @@ App.View.Widgets.Aq_cons.ConsumptionForecastByLandUse = App.View.Widgets.Base.ex
         }
       }
     });
+
+    if (!domain) {
+      var domain = [0,100000000];
+    }
+    this._chartModel.set({yAxisDomain: domain});
 
     this.subviews.push( new App.View.Widgets.Charts.FillBarStacked({
       'opts': this._chartModel,
