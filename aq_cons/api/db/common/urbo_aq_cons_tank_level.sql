e3dddd4ef37efa6a3f8086a87b65a342f5513e71
Alejandro Hall <alejandro@geographica.gs>
2018-02-27 16:59:08 +0100

    Reformat level tank query

--- aq_cons/api/db/common/urbo_aq_cons_tank_level.sql
+++ aq_cons/api/db/common/urbo_aq_cons_tank_level.sql
@@ -5,8 +5,6 @@
 --------------------------------------------------------------------------------
 -- HOW TO USE:
 -- SELECT urbo_aq_cons_tank_level('scope', '2018-01-16T08:00:00.000Z');
--- SELECT urbo_aq_cons_tank_level('scope', '2018-01-16T08:00:00.000Z', FALSE);
--- SELECT urbo_aq_cons_tank_level('scope', '2018-01-16T08:00:00.000Z', TRUE);
 --------------------------------------------------------------------------------
 
 DROP FUNCTION IF EXISTS urbo_aq_cons_tank_level(varchar, timestamp);
@@ -19,14 +17,14 @@ CREATE OR REPLACE FUNCTION urbo_aq_cons_tank_level(
   $$
   DECLARE
     table_tank_measurand text;
-    table_tank text;
+    table_tank_lastdata text;
     table_sector_lastdata text;
     table_sector_agg text;
 
     _q text;
 
   BEGIN
-    table_tank := urbo_get_table_name(id_scope, 'aq_cons_tank');
+    table_tank_lastdata := urbo_get_table_name(id_scope, 'aq_cons_tank', FALSE, TRUE);
     table_tank_measurand := urbo_get_table_name(id_scope, 'aq_cons_tank_measurand');
 
     table_sector_lastdata := urbo_get_table_name(id_scope, 'aq_cons_sector', FALSE, TRUE);
@@ -34,21 +32,33 @@ CREATE OR REPLACE FUNCTION urbo_aq_cons_tank_level(
 
 
     _q = format('
-      UPDATE %s AS measurand
-      SET
-      level = agg.level
-      FROM (SELECT id_entity, (capacity - sum_consumption) AS level
-      FROM %s tank
-      INNER JOIN (
+      WITH consumption_by_tank AS (
         SELECT reftank, SUM(consumption) AS sum_consumption
-        FROM %s cat
-        INNER JOIN (
-          SELECT * FROM %s WHERE "TimeInstant" = %5$L) agg
-          ON cat.id_entity = agg.id_entity GROUP BY reftank) sect
-          ON tank.id_entity = sect.reftank) agg
-      WHERE (measurand.id_entity = agg.id_entity AND measurand."TimeInstant" = %5$L);
+          FROM %1$s sector_lastdata
+          INNER JOIN (
+            SELECT *
+            FROM %2$s
+            WHERE
+              "TimeInstant" > %5$L::timestamp - interval ''1h''
+              AND "TimeInstant" <= %5$L::timestamp) sector_agg
+          ON sector_lastdata.id_entity = sector_agg.id_entity
+          GROUP BY reftank
+      ),
+      _ AS (UPDATE %3$s measurand
+        SET level = measurand.level - consumption_by_tank.sum_consumption
+        FROM consumption_by_tank
+        WHERE "TimeInstant" > %5$L::timestamp - interval ''1h''
+          AND "TimeInstant" <= %5$L::timestamp
+          AND measurand.id_entity = consumption_by_tank.reftank)
+
+      UPDATE %4$s lastdata
+        SET level = lastdata.level - consumption_by_tank.sum_consumption
+        FROM consumption_by_tank
+        WHERE "TimeInstant" > %5$L::timestamp - interval ''1h''
+          AND "TimeInstant" <= %5$L::timestamp
+          AND lastdata.id_entity = consumption_by_tank.reftank;
       ',
-      table_tank_measurand, table_tank, table_sector_lastdata, table_sector_agg, moment
+      table_sector_lastdata, table_sector_agg, table_tank_measurand, table_tank_lastdata, moment
     );
 
 
