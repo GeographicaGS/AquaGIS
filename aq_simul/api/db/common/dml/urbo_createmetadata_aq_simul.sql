d699825ac096569267330df46faec4890f2df5bb
Daniel <danirele10@gmail.com>
2018-04-02 13:32:09 +0200

    Add static variables to metadata.

--- aq_simul/api/db/common/dml/urbo_createmetadata_aq_simul.sql
+++ aq_simul/api/db/common/dml/urbo_createmetadata_aq_simul.sql
@@ -17,11 +17,13 @@ CREATE OR REPLACE FUNCTION urbo_createmetadata_aq_simul(
   DECLARE
   	_tb_categories text;
     _tb_entities text;
+    _tb_variables text;
     _dml_qry text;
   BEGIN
 
   	_tb_categories = urbo_get_table_name('metadata', 'categories');
     _tb_entities = urbo_get_table_name('metadata', 'entities');
+    _tb_variables = urbo_get_table_name('metadata', 'variables');
 
     _dml_qry = format('
       -- CATEGORIES
@@ -37,8 +39,20 @@ CREATE OR REPLACE FUNCTION urbo_createmetadata_aq_simul(
           (''aq_cata.plot_simulation'', ''Simulación de parcelas'', ''aq_simul'', ''aq_cata_plot_simulation'', FALSE, FALSE),
           (''aq_cata.const_simulation'', ''Simulación de construcciones'', ''aq_simul'', ''aq_cata_const_simulation'', FALSE, FALSE),
           (''aq_cata.const_type'', ''Tipos de contrucciones'', ''aq_simul'', ''aq_cata_const_type'', FALSE, FALSE);
+
+      -- STATIC VARIABLES
+      INSERT INTO %s
+          (id_variable, id_entity, entity_field, var_name, var_units, var_thresholds, var_agg, var_reverse, config, table_name, type, mandatory, editable)
+        VALUES
+          (''aq_cata.plot_simulation.tipo'', ''aq_cata.plot_simulation'', ''tipo'', ''Tipo'', NULL, NULL, ''{"NOAGG"}'', FALSE, NULL, NULL, ''catalogue'', FALSE, FALSE),
+          (''aq_cata.plot_simulation.calibre'', ''aq_cata.plot_simulation'', ''calibre'', ''Calibre'', NULL, NULL, ''{"SUM", "AVG", "MIN", "MAX"}'', FALSE, NULL, NULL, ''catalogue'', FALSE, FALSE),
+          (''aq_cata.plot_simulation.n_personas'', ''aq_cata.plot_simulation'', ''n_personas'', ''Número de personas'', NULL, NULL, ''{"SUM", "AVG", "MIN", "MAX"}'', FALSE, NULL, NULL, ''catalogue'', FALSE, FALSE),
+
+          (''aq_cata.const_simulation.floor'', ''aq_cata.const_simulation'', ''floor'', ''Planta'', NULL, NULL, ''{"NOAGG"}'', FALSE, NULL, NULL, ''catalogue'', FALSE, FALSE),
+          (''aq_cata.const_simulation.usage'', ''aq_cata.const_simulation'', ''usage'', ''Uso'', NULL, NULL, ''{"NOAGG"}'', FALSE, NULL, NULL, ''catalogue'', FALSE, FALSE),
+          (''aq_cata.const_simulation.calibre'', ''aq_cata.const_simulation'', ''calibre'', ''Calibre'', NULL, NULL, ''{"SUM", "AVG", "MIN", "MAX"}'', FALSE, NULL, NULL, ''catalogue'', FALSE, FALSE);
     ',
-    _tb_categories, _tb_entities
+    _tb_categories, _tb_entities, _tb_variables
     );
          
     IF isdebug IS TRUE then
