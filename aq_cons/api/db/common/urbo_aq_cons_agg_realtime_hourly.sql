5d33987448a1ee865a7798b57548bc8ddf5894fd
Ignacio Domingo Martínez <ignacio.domingo@geographica.gs>
2018-04-30 13:14:30 +0200

    Make agg data be taken from measurand

--- aq_cons/api/db/common/urbo_aq_cons_agg_realtime_hourly.sql
+++ aq_cons/api/db/common/urbo_aq_cons_agg_realtime_hourly.sql
@@ -21,6 +21,8 @@ CREATE OR REPLACE FUNCTION urbo_aq_cons_agg_realtime_hourly(
   DECLARE
     _t_const_ld text;
     _t_const_ms text;
+	_t_plot_ms text;
+	_t_sector_ms text;
     _t_const_ag text;
     _t_plot_ld text;
     _t_plot_ag text;
@@ -44,9 +46,11 @@ CREATE OR REPLACE FUNCTION urbo_aq_cons_agg_realtime_hourly(
     _t_const_ag := urbo_get_table_name(id_scope, 'aq_cons_const_agg_hour');
 
     _t_plot_ld  := urbo_get_table_name(id_scope, 'aq_cons_plot', FALSE, TRUE);
+    _t_plot_ms := urbo_get_table_name(id_scope, 'aq_cons_plot_measurand');
     _t_plot_ag := urbo_get_table_name(id_scope, 'aq_cons_plot_agg_hour');
 
     _t_sector_ag := urbo_get_table_name(id_scope, 'aq_cons_sector_agg_hour');
+    _t_sector_ms := urbo_get_table_name(id_scope, 'aq_cons_sector_measurand');
 
     _t_aux_ft := urbo_get_table_name(id_scope, 'aq_aux_const_futu');
     _t_aux_lk := urbo_get_table_name(id_scope, 'aq_aux_leak');
@@ -63,49 +67,33 @@ CREATE OR REPLACE FUNCTION urbo_aq_cons_agg_realtime_hourly(
       moment,
       _t_const_ms, moment, moment
     );
-
+    
+    
     _q_plot := format('
       -- PLOT
-      SELECT q1.refplot AS id_entity, ''%s''::timestamp AS "TimeInstant",
-          SUM(q0.consumption) AS consumption, AVG(q0.pressure_agg) AS pressure_agg
-        FROM (
-          SELECT id_entity, "TimeInstant", consumption, pressure_agg
-            FROM %s
-            WHERE "TimeInstant" >= ''%s''
-              AND "TimeInstant" < ''%s''::timestamp + interval ''1 hour''
-        ) q0
-          INNER JOIN (
-            SELECT id_entity, refplot
-              FROM %s
-          ) q1
-            ON q0.id_entity = q1.id_entity
-        GROUP BY q1.refplot
+      SELECT id_entity, ''%s''::timestamp AS "TimeInstant",
+          AVG(flow) AS consumption, AVG(pressure) AS pressure_agg
+        FROM %s
+        WHERE "TimeInstant" >= ''%s''
+          AND "TimeInstant" < ''%s''::timestamp + interval ''1 hour''
+        GROUP BY id_entity
       ',
       moment,
-      _t_const_ag, moment, moment,
-      _t_const_ld
+      _t_plot_ms, moment, moment
     );
-
+    
+    
     _q_sector := format('
       -- SECTOR
-      SELECT q1.refsector AS id_entity, ''%s''::timestamp AS "TimeInstant",
-          SUM(q0.consumption) AS consumption, AVG(q0.pressure_agg) AS pressure_agg
-        FROM (
-          SELECT id_entity, "TimeInstant", consumption, pressure_agg
-            FROM %s
-            WHERE "TimeInstant" >= ''%s''
-              AND "TimeInstant" < ''%s''::timestamp + interval ''1 hour''
-        ) q0
-          INNER JOIN (
-            SELECT id_entity, refsector
-              FROM %s
-          ) q1
-            ON q0.id_entity = q1.id_entity
-        GROUP BY q1.refsector
+      SELECT id_entity, ''%s''::timestamp AS "TimeInstant",
+          AVG(flow) AS consumption, AVG(pressure) AS pressure_agg
+        FROM %s
+        WHERE "TimeInstant" >= ''%s''
+          AND "TimeInstant" < ''%s''::timestamp + interval ''1 hour''
+        GROUP BY id_entity
       ',
       moment,
-      _t_plot_ag, moment, moment,
-      _t_plot_ld
+      _t_sector_ms, moment, moment
     );
 
     _update := 'UPDATE';
