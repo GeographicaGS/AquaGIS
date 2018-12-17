b119d7e49623b310947d0481547ee3263381e11a
rbsolis <bob.solis@gmail.com>
2018-12-17 22:16:16 +0100

    Updated tables.

--- aq_maintenance/api/db/common/ddl/urbo_createtables_aq_maintenance.sql
+++ aq_maintenance/api/db/common/ddl/urbo_createtables_aq_maintenance.sql
@@ -19,19 +19,30 @@ CREATE OR REPLACE FUNCTION urbo_createtables_aq_maintenance(
   RETURNS void AS
   $$
   DECLARE
-    _tb_order text;
+    _tb_arr text[];
+    _checktable text;
+    _pg_geom_idx text;
+    _geom_fld text;
+    _pg_pk text;
+    _pg_tbowner text;
+    _ddl_qry text;
+    _create_tbs text;
+    _time_idx text;
+    _ld_unique text;
+
+    _tb_issues text;
     _tb_status text;
-    _tb_file text;
+    _tb_files text;
   BEGIN
 
-    _tb_order = urbo_get_table_name(id_scope, 'maintenance_order', iscarto);
-    _tb_status = urbo_get_table_name(id_scope, 'maintenance_status', iscarto);
-    _tb_file = urbo_get_table_name(id_scope, 'maintenance_file', iscarto);
+    _tb_issues = urbo_get_table_name(id_scope, 'maintenance_issues');
+    _tb_status = urbo_get_table_name(id_scope, 'maintenance_status');
+    _tb_files = urbo_get_table_name(id_scope, 'maintenance_files');
 
     _tb_arr = ARRAY[
-        _tb_order,
+        _tb_issues,
         _tb_status,
-        _tb_file
+        _tb_files
       ];
 
     _checktable = urbo_checktable_ifexists_arr(id_scope,_tb_arr,TRUE);
@@ -42,108 +53,89 @@ CREATE OR REPLACE FUNCTION urbo_createtables_aq_maintenance(
 
     _geom_fld = 'position';
 
-    _pg_geom_idx = urbo_geom_idx_qry(_geom_fld, _tb_arr);
+    _pg_geom_idx = urbo_geom_idx_qry('position', _tb_arr);
 
     _pg_pk = urbo_pk_qry(_tb_arr);
 
     _pg_tbowner = urbo_tbowner_qry(_tb_arr);
 
-
+    DROP TYPE IF EXISTS order_type;
     CREATE TYPE order_type AS ENUM (
-      'infrastructure update', -- 'actualización de infraestructuras',
-      'maintenance', -- mantenimiento
-      'fault', -- avería
-      'leak' -- fuga
+      'infrastructure update',
+      'maintenance',
+      'fault',
+      'leak'
     );
 
+    DROP TYPE IF EXISTS status_type;
     CREATE TYPE status_type AS ENUM (
-      'registered', -- registrada
-      'in progress', -- en proceso
-      'incident', -- incidencia
-      'fuga' -- fuga
+      'registered',
+      'in progress',
+      'incident',
+      'leak'
     );
 
     _create_tbs = format('
 
-    --ORDERS
-    CREATE TABLE IF NOT EXISTS %s (
-        %I geometry(Point,4326),
-        number character varying(64) NOT NULL,
-        "TimeInstant" timestamp without time zone,
-        type order_type NOT NULL,
-        address text,
-        description text,
-        id_user text,
-        budget double precision,
-        estimated_time double precision,
-        current_status status_type,
-        -- status jsonb,
-        -- files jsonb,
-        id_entity character varying(64) NOT NULL,
-        created_at timestamp without time zone DEFAULT timezone(''utc''::text, now()),
-        updated_at timestamp without time zone DEFAULT timezone(''utc''::text, now())
-    );
+      --ISSUES
+      CREATE TABLE IF NOT EXISTS %s (
+          position geometry(Point,4326),
+          number character varying(64) NOT NULL,
+          "TimeInstant" timestamp without time zone,
+          type order_type NOT NULL,
+          address text,
+          description text,
+          id_user text,
+          budget double precision,
+          estimated_time double precision,
+          current_status status_type,
+          -- status jsonb,
+          -- files jsonb,
+          id_entity character varying(64) NOT NULL,
+          created_at timestamp without time zone DEFAULT timezone(''utc''::text, now()),
+          updated_at timestamp without time zone DEFAULT timezone(''utc''::text, now())
+      );
 
-    --STATUS
-    CREATE TABLE IF NOT EXISTS %s (
-        type status_type NOT NULL,
-        id_user text,
-        id_entity character varying(64) NOT NULL,
-        created_at timestamp without time zone DEFAULT timezone(''utc''::text, now()),
-        updated_at timestamp without time zone DEFAULT timezone(''utc''::text, now())
-    );
+      --STATUS
+      CREATE TABLE IF NOT EXISTS %s (
+          type status_type NOT NULL,
+          id_user text,
+          id_entity character varying(64) NOT NULL,
+          created_at timestamp without time zone DEFAULT timezone(''utc''::text, now()),
+          updated_at timestamp without time zone DEFAULT timezone(''utc''::text, now())
+      );
 
-    --FILES
-    CREATE TABLE IF NOT EXISTS %s (
-        id_entity character varying(64) NOT NULL,
-        created_at timestamp without time zone DEFAULT timezone(''utc''::text, now()),
-        updated_at timestamp without time zone DEFAULT timezone(''utc''::text, now())
-    );
+      --FILES
+      CREATE TABLE IF NOT EXISTS %s (
+          id_entity character varying(64) NOT NULL,
+          created_at timestamp without time zone DEFAULT timezone(''utc''::text, now()),
+          updated_at timestamp without time zone DEFAULT timezone(''utc''::text, now())
+      );
 
     ',
-    _tb_order, _geom_fld,
+    _tb_issues,
     _tb_status,
-    _tb_file,
+    _tb_files
     );
 
-    _time_idx = urbo_time_idx_qry(_tb_arr);
-    _ld_unique = urbo_unique_lastdata_qry(ARRAY[_tb_lastdata_wcn, _tb_catalogue_wcm, _tb_areas]::text[]);
-
-    IF iscarto IS TRUE then
-      _ddl_qry = format('
-        %s
-        %s
-        %s
-        %s
-        -- FOR TORQUE VIEW
-        CREATE VIEW %s AS SELECT * FROM %s;
-        GRANT SELECT ON %s TO publicuser;
-        GRANT SELECT ON %s TO publicuser;
-        ',
-        _create_tbs, _cartodbfy, _time_idx, _ld_unique,
-        _tb_lastdata_view_wcn, _tb_lastdata_wcn,
-        _tb_lastdata_view_wcn, _tb_agg_wcn01
-      );
-    ELSE
-      _ddl_qry = format('
-        %s
-        %s
-        %s
-        %s
-        %s
-        %s
-        %s',
-        _create_tbs, _pg_geom_idx, _pg_pk,
-        _pg_tbowner,_pg_indicators_tb,
-        _time_idx, _ld_unique
-      );
-    END IF;
+    _ddl_qry = format('
+      %s
+      %s
+      %s
+      %s
+      ',
+      _create_tbs,
+      _pg_geom_idx,
+      _pg_pk,
+      _pg_tbowner
+    );
 
     IF isdebug IS TRUE then
       RAISE NOTICE '%', _ddl_qry;
     END IF;
 
     EXECUTE _ddl_qry;
+
 
   END;
   $$ LANGUAGE plpgsql;
