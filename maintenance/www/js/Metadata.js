5b2dba5c8cf1b9f28dff0f175fd89791799d4d78
Antonio Irlandés García <limtrack@gmail.com>
2019-01-14 15:47:49 +0100

    add icon maintenance vertical

--- maintenance/www/js/Metadata.js
+++ maintenance/www/js/Metadata.js
@@ -1,5 +1,5 @@
 
 const additionalMaintenanceInfoCatalog = {
-  'maintenance': { colour: '#5476BB', icon: '../verticals/maintenance/img/menuPanel/app_maintenance.svg' },
+  'maintenance': { colour: '#83ce44', icon: '../verticals/maintenance/img/menuPanel/SC_ic_vertical_Maintenance_S.svg' },
 }
 App.Metadata.prototype._additionalInfoCatalog = Object.assign(App.Metadata.prototype._additionalInfoCatalog, additionalMaintenanceInfoCatalog);
