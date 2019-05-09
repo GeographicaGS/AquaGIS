c3508544e53e780d6df20974bfb51d39b3d0bf19
Antonio Irlandés García <limtrack@gmail.com>
2019-05-09 11:42:54 +0200

    Add data about in metadata.js about "aq_cons.sensor"

--- aq_cons/www/js/Metadata.js
+++ aq_cons/www/js/Metadata.js
@@ -1,5 +1,6 @@
 
 const additionalAQConsInfoCatalog = {
   'aq_cons': { colour: '#5476BB', icon: '../verticals/aq_cons/img/menuPanel/SC_ic_vertical_AQCons_M.svg' },
+  'aq_cons.sensor': { colour: '#9d61f9', icon: '../verticals/aquasig-theme/img/icons/map/sensor-agua.svg' },
 }
 App.Metadata.prototype._additionalInfoCatalog = Object.assign(App.Metadata.prototype._additionalInfoCatalog, additionalAQConsInfoCatalog);
